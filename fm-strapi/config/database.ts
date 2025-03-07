fa0a174ee4d9d46b63cb28fc8f2cae1ac524a9f2
Anna Chukhrova <nyasha1999@mail.ru>
2025-03-07 20:37:38 +0100

    fix db settings strapi

--- fm-strapi/config/database.ts
+++ fm-strapi/config/database.ts
@@ -1,16 +1,15 @@
-export default ({ env }) => {
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client: 'postgres',
-      connection: {
-        connectionString: env('DATABASE_URL'),
-        ssl: {
-          rejectUnauthorized: false,
-        },
-        schema: env('DATABASE_SCHEMA', 'public'),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT', 6543),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: {
+        rejectUnauthorized: false,
       },
-      pool: { min: 1, max: 100 },
-      acquireConnectionTimeout: 60000,
     },
-  };
-};
+  },
+});
